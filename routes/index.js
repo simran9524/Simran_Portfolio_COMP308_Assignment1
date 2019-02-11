edcec8380725bad70d37ca0aa8824eab74d20507
simran9524 <simrankalra9524@gmail.com>
2019-02-10 22:03:31 -0500

    Comp308: adding express js support

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
+/* GET about page. */
+router.get('/aboutme', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
 module.exports = router;
